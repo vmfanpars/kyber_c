9255a6a8878714bc01900f33514e6028a53c242e
Vahid <vahidmoradifanpars@gmail.com>
2024-09-08 09:38:57 +0330

    add duration of functions execution time

--- kyber.c
+++ kyber.c
@@ -20,58 +20,66 @@
 
 int main(void)
 {
-  unsigned int i,j;
+  unsigned int i,j, n=1;
   uint8_t pk[CRYPTO_PUBLICKEYBYTES];
   uint8_t sk[CRYPTO_SECRETKEYBYTES];
   uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
   uint8_t key_a[CRYPTO_BYTES];
   uint8_t key_b[CRYPTO_BYTES];
 
-  int start, end;
-
-  for(i=0;i<1;i++) {
+  for(i=0;i<n;i++) {
 
     // Key-pair generation
+    int start, end;
     start = clock();
     crypto_kem_keypair(pk, sk);
     end = clock();
 
-    printf("Public Key char: ");
+    printf("Public Key char:\n");
     for(j=0;pk[j] || pk[j+1] || pk[j+2];j++)
       printf("%c",pk[j]);
     printf("\nlen pk char= %u\n\n",CRYPTO_PUBLICKEYBYTES);
 
-    printf("Public Key: ");
+    printf("Public Key:\n");
     for(j=0;j<CRYPTO_PUBLICKEYBYTES;j++)
       printf("%02x",pk[j]);
     printf("\nlen pk= %u\n\n",j);
     
-    printf("Secret Key: ");
+    printf("Secret Key:\n");
     for(j=0;j<CRYPTO_SECRETKEYBYTES;j++)
       printf("%02x",sk[j]);
     printf("\nlen sk= %u\n\n",j);
 
-    printf("***   time pk and sk= %d milli second\n\n",(end-start));
+    printf("*** Duration of Public and Private keys generation: %d milli second\n\n",(end-start));
     
     // Encapsulation
+    start = clock();
     crypto_kem_enc(ct, key_a, pk);
+    end = clock();
 
-    printf("Ciphertext: ");
+    printf("Ciphertext:\n");
     for(j=0;j<CRYPTO_CIPHERTEXTBYTES;j++)
       printf("%02x",ct[j]);
     printf("\nlen ct= %u\n\n",j);
 
-    printf("Shared Secret A: ");
+    printf("Shared Secret A:\n");
     for(j=0;j<CRYPTO_BYTES;j++)
       printf("%02x",key_a[j]);
     printf("\nlen ssa= %u\n\n",j);
 
+    printf("*** Duration of Ciphertext and Shared Secret A generation: %d milli second\n\n",(end-start));
+
     // Decapsulation
+    start = clock();
     crypto_kem_dec(key_b, ct, sk);
-    printf("Shared Secret B: ");
+    end = clock();
+
+    printf("Shared Secret B:\n");
     for(j=0;j<CRYPTO_BYTES;j++)
       printf("%02x",key_b[j]);
     printf("\nlen ssb= %u\n\n",j);
+
+    printf("*** Duration of Shared Secret B generation: %d milli second\n\n",(end-start));
 
     for(j=0;j<CRYPTO_BYTES;j++) {
       if(key_a[j] != key_b[j]) {
@@ -81,14 +89,10 @@ int main(void)
     }
 
     // Decapsulation of invalid (random) ciphertexts
-    int start1, end1;
-    start1 = clock();
-
     randombytes(ct, KYBER_CIPHERTEXTBYTES); 
     crypto_kem_dec(key_a, ct, sk);
-    end1 = clock();
-    printf("***   time pssa= %d milli second\n\n",(end1-start1));
-    printf("Pseudorandom shared Secret A: ");
+    
+    printf("Pseudorandom Shared Secret A:\n");
     for(j=0;j<CRYPTO_BYTES;j++)
       printf("%02x",key_a[j]);
     printf("\nlen pssa= %u\n\n",j);
@@ -99,7 +103,6 @@ int main(void)
   char* time_string = ctime(&current_time);
     
   printf("finish at: %s", time_string);
-  printf("end\n");
 
   return 0;
 }
